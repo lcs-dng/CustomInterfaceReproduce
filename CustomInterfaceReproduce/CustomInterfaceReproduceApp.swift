56acfdd25d657320b46a7e5f3604a31cdeca9e94
Dexter Ng <DNg@lcs.on.ca>
2023-11-16 09:45:19 -0500

    Initial Commit

---
+++ CustomInterfaceReproduce/CustomInterfaceReproduceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomInterfaceReproduceApp.swift
+//  CustomInterfaceReproduce
+//
+//  Created by Dexter Ng on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct CustomInterfaceReproduceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
